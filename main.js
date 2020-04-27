71882b608f3dd1e7df43578e3f84ee405f6fe0aa
mansouri <mansouri@email.arizona.edu>
2020-04-27 14:56:04 -0700

    Game Logic

--- main.js
+++ main.js
@@ -19,9 +19,34 @@ var leftPressed = false;
 var dx = 2;
 var dy = -2;
 
+// Brick Variables
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffSetTop = 30;
+var brickOffSetLeft = 30;
+
+// Score Variables
+var score = 0;
+var lives = 3;
+
+// Creates Bricks Array
+var bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+
 // Event Listeners for moving the paddle left or right
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+// Event Listener for mouse
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 //  Functions for moving paddle;
 function keyDownHandler(e) {
@@ -31,6 +56,7 @@ function keyDownHandler(e) {
         leftPressed = true;
     }
 }
+
 function keyUpHandler(e) {
     if (e.key === "Right" || e.key == "ArrowRight") {
         rightPressed = false;
@@ -38,6 +64,48 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
+    }
+}
+
+// Detects if the ball has hit any of the bricks
+function collisionDetection() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    // displays message if all bricks are destroyed
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("You Win, Congratulations!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Updates score
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " + score, 8, 20);
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
+
 // Draw Function every 10 miliseconds
 function drawBall() {
     ctx.beginPath();
@@ -55,17 +123,60 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+// Function that loops through all bricks in array and Draws them
+function drawBricks() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            // deletes the brick when hit
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffSetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffSetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
 function draw() {
     // Makes the trail of the ball moving go away
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
-    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
-        dy = -dy;
-    }
+    drawScore();
+    drawLives();
+    collisionDetection();
     if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx;
     }
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            lives--;
+            if (!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+        }
+    }
+
     if (rightPressed) {
         paddleX += 7;
         if (paddleX + paddleWidth > canvas.width) {
@@ -79,12 +190,7 @@ function draw() {
     }
     x += dx;
     y += dy;
+    requestAnimationFrame(draw);
 }
 
-
-
-
-
-
-
-setInterval(draw, 10);
+draw();
